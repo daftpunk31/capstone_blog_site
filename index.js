3b0033518dfe42c285ce78448b4fa7b9922daf15
daftpunk <suryaprakashk747@gmail.com>
2024-08-11 22:00:32 +0530

    Deploy-test-1
    Trying to deploy node js app for free

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
